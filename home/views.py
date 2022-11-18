84b867e5a28fd45637a23b145d65754c52c199fa
Gordey13dek <116313862+Gordey13dek@users.noreply.github.com>
2022-11-18 10:19:03 +0200

    Update views.py
    Change name of page_title

--- home/views.py
+++ home/views.py
@@ -3,25 +3,25 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html', {
-        'page_title': 'Home'
+        'page_title': 'Головна'
     })
 
 
 def about(request):
     return render(request, 'home/about.html', {
-        'page_title': 'About'
+        'page_title': 'Про нас'
     })
 
 
 def blog(request):
     return render(request, 'home/blog.html', {
-        'page_title': 'Blog'
+        'page_title': 'Блог'
     })
 
 
 def contact(request):
     return render(request, 'home/contact.html', {
-        'page_title': 'Contact'
+        'page_title': 'Контакти'
     })
 
 
